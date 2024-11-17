0ad5da6edf76c3a6b26be4627cf5a0c18e5d0e72
ania0 <43790803+ania0@users.noreply.github.com>
2024-11-17 14:39:46 +0100

    kodowanie, ale nie jestem pewna czy dziala

--- nowaWersja.py
+++ nowaWersja.py
@@ -1,80 +1,99 @@
-class GF256:
-    def __init__(self):
-        self.wielomian = 0x11B  # Nierozkładalny wielomian dla ciała GF(2^8)
-        self.size = 256
-        self.exp_table = [0] * self.size
-        self.log_table = [0] * self.size
-        self.known_polynomials = {
-            0: [1, 1],  # m0
-            1:[1, 0, 0, 0, 1, 1, 1, 0, 1],  # m1
-            3: [1, 0, 1, 1, 1, 0, 1, 1, 1],  # m3
-            5: [1, 1, 1, 1, 1, 0, 0, 1, 1],  # m5
-            7: [1, 0, 1, 1, 0, 1, 0, 0, 1],  # m7
-            9: [1, 1, 0, 1, 1, 1, 1, 0, 1],  # m9
-            11:[1, 1, 1, 1, 0, 0, 1, 1, 1],  # m11
-            13: [1, 0, 0, 1, 0, 1, 0, 1, 1],  # m13
-            15: [1, 1, 1, 0, 1, 0, 1, 1, 1],  # m15
-            17: [1, 0, 0, 1, 1],  # m17
-            19: [1, 0, 1, 1, 0, 0, 1, 0, 1],  # m19
-            21:[1, 1, 0, 0, 0, 1, 0, 1, 1],  # m21
-        }
+# Define the Galois Field GF(2^8) parameters
+# Primitive polynomial for GF(2^8): x^8 + x^4 + x^3 + x^2 + 1
+primitive_polynomial = 0x11D  # This is the binary representation of the polynomial
+
+# Precompute the logarithm and exponential tables for GF(2^8)
+exp_table = [0] * 256
+log_table = [0] * 256
+
+x = 1
+for i in range(255):
+    exp_table[i] = x
+    log_table[x] = i
+    x <<= 1
+    if x & 0x100:  # If x is greater than 255, reduce it
+        x ^= primitive_polynomial
+
+# Define addition and multiplication in GF(2^8)
+def galois_add(a, b):
+    return a ^ b  # XOR for addition in GF(2)
+
+def galois_multiply(a, b):
+    if a == 0 or b == 0:
+        return 0
+    return exp_table[(log_table[a] + log_table[b]) % 255]
+
+# Minimal polynomials for BCH code
+minimal_polynomials = {
+    0: [1, 1],  # m0
+    1: [1, 0, 0, 0, 1, 1, 1, 0, 1],  # m1
+    3: [1, 0, 1, 1, 1, 0, 1, 1, 1],  # m3
+    5: [1, 1, 1, 1, 1, 0, 0, 1, 1],  # m5
+    7: [1, 0, 1, 1, 0, 1, 0, 0, 1],  # m7
+    9: [1, 1, 0, 1, 1, 1, 1, 0, 1],  # m9
+    11: [1, 1, 1, 1, 0, 0, 1, 1, 1],  # m11
+    13: [1, 0, 0, 1, 0, 1, 0, 1, 1],  # m13
+    15: [1, 1, 1, 0, 1, 0, 1, 1, 1],  # m15
+    17: [1, 0, 0, 1, 1],  # m17
+    19: [1, 0, 1, 1, 0, 0, 1, 0, 1],  # m19
+    21: [1, 1, 0, 0, 0, 1, 0, 1, 1],  # m21
+}
+
+class BCHCoder:
+    def __init__(self, n, k, minimal_polynomials):
+        self.n = n  # Length of the codeword
+        self.k = k  # Length of the message
+        self.t = (n - k) // 2  # Error correction capability
+        self.minimal_polynomials = minimal_polynomials
         self.generator_polynomial = self.generate_generator_polynomial()
-        self._create_galois_tables()
-
-    def _create_galois_tables(self):
-        # Tworzenie tabeli wykładników i logarytmów
-        x = 1
-        for i in range(self.size - 1):
-            self.exp_table[i] = x
-            self.log_table[x] = i
-            x <<= 1
-            if x & 0x100:  # Jeśli wartość przekracza 8 bitów, stosujemy modulo
-                x ^= self.wielomian
-        self.exp_table[self.size - 1] = 1  # 1 na końcu dla cykliczności
-
-    def galois_multiply(self, a, b):
-        if a == 0 or b == 0:
-            return 0
-        return self.exp_table[(self.log_table[a] + self.log_table[b]) % (self.size - 1)]
+        self.generator_matrix = self.generate_generator_matrix()
 
     def multiply_polynomials(self, poly1, poly2):
-        # Mnożenie dwóch wielomianów
+        """Multiply two polynomials in GF(2)."""
         result = [0] * (len(poly1) + len(poly2) - 1)
-        for i in range(len(poly1)):
-            for j in range(len(poly2)):
-                result[i + j] ^= self.galois_multiply(poly1[i], poly2[j])
+        for i, coef1 in enumerate(poly1):
+            for j, coef2 in enumerate(poly2):
+                result[i + j] ^= galois_multiply(coef1, coef2)
         return result
 
     def generate_generator_polynomial(self):
-        # Generowanie wielomianu generującego przez mnożenie wszystkich wielomianów minimalnych
-        generator_poly = [1]  # Rozpoczynamy od wielomianu 1
-        for poly in self.known_polynomials.values():
-            generator_poly = self.multiply_polynomials(generator_poly, poly)
+        """Generate the generator polynomial as the product of minimal polynomials."""
+        generator_poly = [1]  # Initial polynomial: 1
+        for i in range(1, 2 * self.t + 1, 2):  # Use m1, m3, ..., m21
+            if i in self.minimal_polynomials:  # Check for existence of key
+                generator_poly = self.multiply_polynomials(generator_poly, self.minimal_polynomials[i])
         return generator_poly
 
-def encode(self, data):
-    encoded_data = []
-    part_length = 171  # Długość słowa danych
+    def generate_generator_matrix(self):
+        """Generate the Generator Matrix G."""
+        P = [[0] * (self.n - self.k) for _ in range(self.k)]
+        for i in range(self.k):
+            for j in range(self.n - self.k):
+                P[i][j] = self.generator_polynomial[(i + j) % len(self.generator_polynomial)]
+        G = [[0] * self.n for _ in range(self.k)]
+        for i in range(self.k):
+            G[i][i:self.k] = [1] + [0] * (self.k - i - 1)
+            G[i][self.k:] = P[i]
+        return G
 
-    for i in range(0, len(data), part_length):
-        part = data[i:i + part_length]
-        if len(part) < part_length:
-            part += [0] * (part_length - len(part))  # Uzupełnianie zerami
+    def encode(self, message):
+        """Encode the message using the Generator Matrix G."""
+        if len(message) != self.k:
+            raise ValueError(f"The message must be exactly {self.k} bits.")
+        codeword = [0] * self.n
+        for i in range(self.k):
+            for j in range(self.n):
+                codeword[j] ^= galois_multiply(message[i], self.generator_matrix[i][j])
+        return codeword
 
-        padded_data = part + [0] * (len(self.generator_polynomial) - 1)
-        for j in range(part_length):
-            coeff = padded_data[j]
-            if coeff != 0:
-                for k in range(len(self.generator_polynomial)):
-                    padded_data[j + k] ^= self.galois_multiply(coeff, self.generator_polynomial[k])
+# Parameters
+n = 255
+k = 171
 
-        # Dodajemy część danych zakodowanych jako końcowe symbole (część kontrolna)
-        encoded_data.extend(part + padded_data[part_length:])
-    return encoded_data
+# Create BCHCoder object
+bch = BCHCoder(n, k, minimal_polynomials)
 
-# Przykładowe użycie
-gf = GF256()
-data = [1]*255  # Słowo kodowe o długości 255
-encoded = gf.encode(data)
-
-print("Zakodowane dane:", encoded)
+# Encode a message of ones
+message = [1] * k  # Message to encode
+encoded_message = bch.encode(message)
+print("Encoded message:", encoded_message)
